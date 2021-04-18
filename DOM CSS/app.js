8663d9697f81d7416a8e3d822eb998fb40319a65
SpecialAgentV <specialagentv@gmail.com>
2021-04-18 18:49:29 -0400

    add & remove buttons

--- DOM CSS/app.js
+++ DOM CSS/app.js
@@ -1,14 +1,24 @@
 const input = document.querySelector('input');
-const p = document.querySelector('p.descript');
-const button = document.querySelector('button');
-const add = document.querySelector('input.add');
+const p = document.querySelector('p.description');
+const button = document.querySelector('descriptionButton');
+const addInput = document.querySelector('input.add');
 const addButton = document.querySelector('button.addButton');
+const removeButton = document.querySelector('button.removeItem');
 
 button.addEventListener ('click', () => {
   p.textContent = input.value + ':';            
    });
 
-addButton.addEventListener('click'. () => {
-                           let li = document.createElement('li');
-li.textContent = add.value;
-                           });
+addButton.addEventListener('click', () => {
+  let ul = document.getElementByTagName ('ul')[0];
+  let li = document.createElement('li');
+  li.textContent = addInput.value;
+  ul.appendChild(li);
+  li.textContent = ' ';
+});
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementByTagName ('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
+  
+});

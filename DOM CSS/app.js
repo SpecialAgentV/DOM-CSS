a94b9cbf015b430418c36d95fed5b8595c38aaa3
SpecialAgentV <specialagentv@gmail.com>
2021-04-22 12:18:00 -0400

    event.target on mouseover

--- DOM CSS/app.js
+++ DOM CSS/app.js
@@ -10,6 +10,8 @@ const addButton = document.querySelector('button.addBut');
 
 const removeButton = document.querySelector('button.removeItem');
 
+const zoomHover = document.getElementsByTagName('li');
+
 toggleList.addEventListener('click', ()=> {
 if (listDiv.style.display == 'none'){
   toggleList.textContent = 'Hide list';
@@ -22,7 +24,19 @@ if (listDiv.style.display == 'none'){
 
 descriptionButton.addEventListener ('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';            
-   });
+});
+
+
+listDiv.addEventListener('mouseover', (event) => {
+  console.log (event.target.tagName);
+  if (event.target.tagName == 'LI')
+  event.target.style.fontSize= '200%';
+});
+listDiv.addEventListener('mouseout', (event) => {
+  if (event.target.tagName == 'LI')
+  event.target.style.fontSize= '100%'; 
+});
+
 
 addButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName ('ul')[0];
---
+++ DOM CSS/temp.js
@@ -0,0 +1,19 @@
+
+function say(){
+    console.log();
+}
+function ex (func, arg){
+    func(arg);
+}
+ex((x) => {
+    console.log(x);
+}, 'Ydadao');
+
+window.setTimeout((v) => {
+    console.log(v);
+}, 3000, 'eat butt');
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+  }
+window.setTimeout (add, 5000, 2,2)
